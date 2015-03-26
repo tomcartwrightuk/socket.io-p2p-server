4ba021dc0720809364b3e3d33bb76d80f4cee23f
Tom Cartwright <tecartwright@gmail.com>
2015-03-26 20:30:59 +0000

    Can be used as middleware or within connection block

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ var clients = {}
 module.exports.clients = clients
 module.exports.Server = p2pSocket
 
-function p2pSocket (socket, next) {
-  socket.emit('numClients', Object.keys(clients).length)
-  socket.broadcast.emit('connected_peer', socket.id)
-
+function p2pSocket (socket, next, room) {
   clients[socket.id] = socket
+  if (typeof room === 'object') {
+    var connectedClients = socket.adapter.rooms[room.name]
+  } else {
+    var connectedClients = clients
+  }
+  socket.emit('numClients', Object.keys(connectedClients).length - 1)
+
   socket.on('disconnect', function () {
     delete clients[socket.id]
     debug('Client gone (id=' + socket.id + ').')
   })
 
   socket.on('offers', function (data) {
-    Object.keys(clients).forEach(function (clientId, i) {
+    // send offers to everyone in a given room
+    Object.keys(connectedClients).forEach(function (clientId, i) {
       var client = clients[clientId]
       if (client !== socket) {
         var offerObj = data.offers[i]
@@ -28,8 +33,10 @@ function p2pSocket (socket, next) {
 
   socket.on('peer-signal', function (data) {
     var toPeerId = data.toPeerId
+    console.log("To peer id %s", toPeerId);
+    console.log(Object.keys(connectedClients));
     var client = clients[toPeerId]
     client.emit('peer-signal', data)
   })
-  next()
+  typeof next === 'function' && next()
 }
